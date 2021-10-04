448354b79a5bfc6c6a4712e4d0c6b803700af641
SyarifAbdurrachman <12200453@bsi.ac.id>
2021-10-04 14:11:03 +0700

    Tugas Pertemuan 3

--- app/Views/halaman/daftar_member.php
+++ app/Views/halaman/daftar_member.php
@@ -1,10 +1,16 @@
 <html>
     <head>
-        <title>Daftar Member</title>
+        <title>Pendaftaran Member Perpustakaan</title>
     </head>
     <body>
-        <h1>Hai.. kamu mau menjadi member?</h1>
+        <h1>Hai.... Kamu mau menjadi member?</h1>
         <p1>Isikan data diri kamu dulu ya agar kami kenal dengan member kami</p1>
+        <ol>
+            <li>Nama Anda   :</li>
+            <li>Kelas       :</li>
+            <li>NIM         :</li>
+        </ol>
+
+
     </body>
-    
 </html>
