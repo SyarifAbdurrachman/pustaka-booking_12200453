27c634a7219a73f27b5fd974dbdffee719b1bc10
12200453SyarifAbdurrachman <90823590+SyarifAbdurrachman@users.noreply.github.com>
2021-11-18 09:45:42 +0700

    Tugas Pertemuan 5

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -2,48 +2,49 @@
 namespace App\Controllers;
 
 use App\Models\Pengguna_12200453;
-use Config\Services;
 
 class Login extends BaseController{
-   
- 
+
+    
     public function cekLogin(){
         $email = $this->request->getPost('email');
         $sandi = $this->request->getPost('sandi');
-        
-        $v = $this->validate([
+
+        $v =  $this->validate([
             'email' => 'required',
             'sandi' => 'required'
-        ], [
-            'email'=>[ 
-                'required'    => 'Email tidak boleh kosong'
-            ],
-            'sandi' => [
-                'required'    => 'Sandi tidak boleh kosong'
-            ]
-        ]);
+         ], [
+             'email'=>[
+                 'required'     =>'Email tidak boleh kosong'
+             ],
+             'sandi'=>[
+                 'required'     =>'Sandi tidak boleh kosong'
+             ]
+         ]);
+        
 
-        $this->session->set('email', $email);
-        $this->session->set('sandi', $sandi);
+         $this->session->set('email', $email);
+         $this->session->set('sandi', $sandi);
 
-        if( $v == false ){ 
-            $this->session->setFlashdata('validator', $this->validator);
+         
+         if($v == false){
+             $this->session->setFlashdata(
+                 'validator', $this->validator
+             );
             return redirect()->to('/login');
-        }else{ 
+         }else{
+            $vl = (new Pengguna_12200453())->cekLogin($email, $sandi); 
 
-            $vl = (new Pengguna())->cekLogin($email, $sandi);
-
-            if($vl == null){
+            if ($vl == null){
                 return redirect()->to('/login')->with('error', 'User dan sandi salah');
             }else{
                 $this->session->set('sudahLogin', true);
-                return redirect()->to('/beranda');
+             return redirect()->to('/beranda');
             }
-        }
+           
+         }
     }
-
     public function beranda(){
-        
+
     }
- 
 }
